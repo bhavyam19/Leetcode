19ce9c033147b96933ab2fb01c5c262b958e4037
bhavyam19 <bhavyamgarg19@gmail.com>
2026-07-05 21:43:25 +0530

    Time: 0 ms (100%), Space: 128.4 MB (54.18%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -5,12 +5,12 @@ public:
         int posIndex = 0;
         int negIndex = 1;
         for(int i=0;i<nums.size();i++){
-            if (nums[i]<0) {
-                ans[negIndex] = nums[i];
-                negIndex+=2;
-            }else{
+            if (nums[i]>0) {
                 ans[posIndex] = nums[i];
                 posIndex+=2;
+            }else{
+                ans[negIndex] = nums[i];
+                negIndex+=2;
             }
         }
         return ans;
